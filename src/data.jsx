56f9219d507e9dfe9b445394a6aed30da153a89d
chandru90 <iochandru90@gmail.com>
2023-11-10 20:27:24 +0530

    commit4

--- src/data.jsx
+++ src/data.jsx
@@ -1,5 +1,5 @@
 import { nanoid } from 'nanoid';
-import { FaHtml5, FaJs, FaReact } from 'react-icons/fa';
+import { FaHtml5, FaJs, FaReact,FaNodeJs,FaGlobe,FaFeatherAlt,FaBity,FaGithub} from 'react-icons/fa';
 
 export const links = [
   { id: nanoid(), href: '#home', text: 'home' },
@@ -13,31 +13,36 @@ export const skills = [
     id: nanoid(),
     title: 'HTML&CSS',
     icon: <FaHtml5 className='h-16 w-16 text-emerald-500' />,
-    text: 'Highly skilled in HTML & CSS, adeptly crafting visually appealing and responsive websites for optimal user experiences.',
   },
   {
     id: nanoid(),
     title: 'Javascript',
     icon: <FaJs className='h-16 w-16 text-emerald-500' />,
-    text: 'Expertise in JavaScript, building interactive and dynamic web applications with a focus on seamless user interactions and functionality',
   },
   {
     id: nanoid(),
     title: 'React',
     icon: <FaReact className='h-16 w-16 text-emerald-500' />,
-    text: 'Advanced proficiency in React, developing efficient and interactive front-end applications with a strong emphasis on component-based architecture.',
   },
   {
     id: nanoid(),
-    title: 'HTML&CSS',
-    icon: <FaHtml5 className='h-16 w-16 text-emerald-500' />,
-    text: 'Highly skilled in HTML & CSS, adeptly crafting visually appealing and responsive websites for optimal user experiences.',
+    title: 'NodeJS',
+    icon: <FaNodeJs className='h-16 w-16 text-emerald-500' />,
   },
   {
     id: nanoid(),
-    title: 'HTML&CSS',
-    icon: <FaHtml5 className='h-16 w-16 text-emerald-500' />,
-    text: 'Highly skilled in HTML & CSS, adeptly crafting visually appealing and responsive websites for optimal user experiences.',
+    title: 'MangoDB',
+    icon: <FaFeatherAlt className='h-16 w-16 text-emerald-500' />,
+  },
+  {
+    id: nanoid(),
+    title: 'Redux',
+    icon: <FaBity className='h-16 w-16 text-emerald-500' />,
+  },
+  {
+    id: nanoid(),
+    title: 'Github',
+    icon: <FaGithub className='h-16 w-16 text-emerald-500' />,
   },
 ];
 
